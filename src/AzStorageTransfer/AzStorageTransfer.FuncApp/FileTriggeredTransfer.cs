9e1ca5b0105df779d61c5dbb205f0b80ade1b8b7
JamesB-87 <74327678+JamesB-87@users.noreply.github.com>
2023-03-07 17:21:32 +0000

    Update FileTriggeredTransfer.cs

--- src/AzStorageTransfer/AzStorageTransfer.FuncApp/FileTriggeredTransfer.cs
+++ src/AzStorageTransfer/AzStorageTransfer.FuncApp/FileTriggeredTransfer.cs
@@ -1,8 +1,10 @@
 using Amazon.S3;
+using Amazon.Util.Internal;
 using Microsoft.Azure.Storage.Blob;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -20,21 +22,41 @@ namespace AzStorageTransfer.FuncApp
         [FunctionName(nameof(FileTriggeredTransfer))]
         public async Task Run(
             [BlobTrigger("%LiveContainer%/{name}", Connection = nameof(Config.DataStorageConnection))]
-            ICloudBlob myBlob, 
-            string name, 
+            ICloudBlob myBlob,
+            string name,
             ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Properties.Length} Bytes");
 
+
+
             using (var ms = new MemoryStream())
             {
                 // Download blob content to stream
-                await myBlob.DownloadToStreamAsync(ms);                
+                await myBlob.DownloadToStreamAsync(ms);
                 ms.Seek(0, SeekOrigin.Begin);
-                
+
                 // Upload stream to S3
-                await this.amazonS3.UploadObjectFromStreamAsync(Config.Aws.BucketName, name, ms, new Dictionary<string, object>());
+                //await this.amazonS3.UploadObjectFromStreamAsync(Config.Aws.BucketName, name, ms, new Dictionary<string, object>());
+
+                var transfer = new Amazon.S3.Transfer.TransferUtility(amazonS3);
+                var request = new Amazon.S3.Transfer.TransferUtilityUploadRequest
+                {
+                    BucketName = Config.Aws.BucketName,
+                    Key = name,
+                    InputStream = ms,
+                    
+                };
+                
+                request.Headers.ContentMD5 = myBlob.Properties.ContentMD5;
+
+                IDictionary<string, object> additionalProperties = new Dictionary<string, object>();
+                InternalSDKUtils.ApplyValues(request, additionalProperties);
+                
+                transfer.Upload(request);
             }
         }
+
     }
+    
 }
